ca87701d27bcc703fcb4b6db39f10eda6a023e77
JudsonMorgan <judeezeh2012@gmail.com>
2025-04-11 19:51:54 +0100

    added a sql table

--- my_project/models/marts/bike_metrics.sql
+++ my_project/models/marts/bike_metrics.sql
@@ -1,5 +1,9 @@
-SELECT 
-  location_city,
-  COUNT(*) AS trip_count,
+SELECT
+    location_city,
+    location_country,
+    COUNT(DISTINCT id) AS total_systems,
+    COUNT(*) AS record_count,
+    AVG(location_latitude) AS avg_latitude,
+    AVG(location_longitude) AS avg_longitude
 FROM {{ ref('stg_bike_data') }}
-GROUP BY location_city
+GROUP BY location_city, location_country
